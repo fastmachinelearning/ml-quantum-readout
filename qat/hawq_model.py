b54767f53c6f3d9a27504f755fa3577e28f5a20b
Javier Campos <jivcampos8@gmail.com>
2023-02-08 18:44:25 -0600

    Add qat training scripts

---
+++ qat/hawq_model.py
@@ -0,0 +1,44 @@
+import torch.nn as nn
+
+from hawq.utils.quantization_utils.quant_modules import QuantLinear, QuantAct 
+
+
+csr = range(500, 1500)
+sr = len(csr)
+hn = sr * 2 * 1
+
+
+class HawqClassifier(nn.Module):
+    def __init__(self, model):
+        super(HawqClassifier, self).__init__()
+
+        self.quant_input = QuantAct(activation_bit=12)
+        self.q_relu1 = QuantAct(activation_bit=12)
+        self.q_relu2 = QuantAct(activation_bit=12)
+
+        layer = getattr(model, 'linear1')
+        hawq_layer = QuantLinear(weight_bit=6, bias_bit=8)
+        hawq_layer.set_param(layer)
+        setattr(self, 'linear1', hawq_layer)
+
+        layer = getattr(model, 'linear2')
+        hawq_layer = QuantLinear(weight_bit=6, bias_bit=8)
+        hawq_layer.set_param(layer)
+        setattr(self, 'linear2', hawq_layer)
+
+        self.relu = nn.ReLU()
+        self.bn = nn.BatchNorm1d(int(hn / 8), affine=True)
+
+    def forward(self, sig):
+        x, p_sf = self.quant_input(sig)
+
+        x = self.linear1(x, p_sf)
+        x = self.relu(x)
+        x, p_sf = self.q_relu1(x, p_sf)
+
+        x = self.bn(x)
+
+        x = self.linear2(x, p_sf)
+        x = self.relu(x)
+        x, p_sf = self.q_relu2(x, p_sf)
+        return x
---
+++ qat/model.py
@@ -0,0 +1,26 @@
+import torch.nn as nn
+
+csr = range(500, 1500)
+sr = len(csr)
+hn = sr * 2 * 1
+
+
+class Classifier(nn.Module):
+    def __init__(self):
+        super(Classifier, self).__init__()
+
+        self.linear1 = nn.Linear(sr * 2, int(hn / 8))
+        self.relu1 = nn.ReLU()
+        self.bn = nn.BatchNorm1d(int(hn / 8), affine=True)
+
+        self.linear2 = nn.Linear(int(hn / 8), 2)
+        self.relu2 = nn.ReLU()
+
+    def forward(self, sig):
+        x = self.linear1(sig)
+        x = self.relu1(x)
+        x = self.bn(x)
+
+        x = self.linear2(x)
+        x = self.relu2(x)
+        return x
