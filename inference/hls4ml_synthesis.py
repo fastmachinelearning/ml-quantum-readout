d569464bf7b16440e557c429ddf136458f35de6c
Javier Campos <jivcampos8@gmail.com>
2023-02-09 15:21:31 -0600

    Update HLS precision

--- inference/hls4ml_synthesis.py
+++ inference/hls4ml_synthesis.py
@@ -3,11 +3,16 @@ import argparse
 import numpy as np
 import hls4ml
 
+import sys
+sys.path.append('..')
+
 import torch
 import torch.nn as nn
 import torch.utils.data
 
 from utils.data import test_data
+from utils.config import print_dict
+from utils.hls import evaluate_hls
 
 
 csr = range(500, 1500)
@@ -37,76 +42,81 @@ class Classifier(nn.Module):
 
 
 def get_static_config():
-    precision = 'ap_fixed<24,10>'
-    reduced_precision = 'ap_fixed<24,10>'
     reusefactor = args.reuse_factor
+    weight_precision = 'ap_fixed<12,2>'
+    result_precision = 'ap_fixed<24,10>'
+    reduced_result_precision = 'ap_fixed<12,4>'
 
-    config ={}
+    hls_config ={}
 
-    config['Model'] = {}
-    config['Model']['ReuseFactor'] = reusefactor
-    config['Model']['Precision'] = reduced_precision
-    config['Model']['Strategy'] = 'resource'
+    hls_config['Model'] = {}
+    hls_config['LayerName'] = {}
+    hls_config['Model']['ReuseFactor'] = reusefactor
+    hls_config['Model']['Precision'] = "ap_fixed<10,10>"
+    hls_config['Model']['Strategy'] = 'resource'
+
     ### layers
-    config['Model']['bn'] = {}
-    config['Model']['linear1'] = {}
-    config['Model']['linear2'] = {}
-    config['Model']['bn']['Precision'] = {}
-    config['Model']['linear1']['Precision'] = {}
-    config['Model']['linear2']['Precision'] = {}
+    hls_config['LayerName']['bn'] = {}
+    hls_config['LayerName']['linear1'] = {}
+    hls_config['LayerName']['linear2'] = {}
+    hls_config['LayerName']['bn']['Precision'] = {}
+    hls_config['LayerName']['linear1']['Precision'] = {}
+    hls_config['LayerName']['linear2']['Precision'] = {}
+
     # weight
-    config['Model']['bn']['Precision']['scale'] = precision
-    config['Model']['linear1']['Precision']['weight'] = reduced_precision
-    config['Model']['linear2']['Precision']['weight'] = reduced_precision
+    hls_config['LayerName']['bn']['Precision']['scale'] = weight_precision
+    hls_config['LayerName']['linear1']['Precision']['weight'] = weight_precision
+    hls_config['LayerName']['linear2']['Precision']['weight'] = weight_precision
+
     # bias
-    config['Model']['bn']['Precision']['bias'] = precision
-    config['Model']['linear1']['Precision']['bias'] = reduced_precision
-    config['Model']['linear2']['Precision']['bias'] = reduced_precision
+    hls_config['LayerName']['bn']['Precision']['bias'] = weight_precision
+    hls_config['LayerName']['linear1']['Precision']['bias'] = weight_precision
+    hls_config['LayerName']['linear2']['Precision']['bias'] = weight_precision
+
     # accum 
-    config['Model']['bn']['Precision']['accum_t'] = 'ap_fixed<24, 10>'
-    config['Model']['linear1']['Precision']['accum_t'] = 'ap_fixed<24, 10>'
-    config['Model']['linear2']['Precision']['accum_t'] = 'ap_fixed<24, 10>'
-    config['Model']['bn']['accum_t'] = 'ap_fixed<24, 16>'
-    config['Model']['linear1']['accum_t'] = 'ap_fixed<24, 16>'
-    config['Model']['linear2']['accum_t'] = 'ap_fixed<24, 16>'
+    hls_config['LayerName']['bn']['Precision']['accum_t'] = result_precision
+    hls_config['LayerName']['linear1']['Precision']['accum_t'] = result_precision
+    hls_config['LayerName']['linear2']['Precision']['accum_t'] = result_precision
+    hls_config['LayerName']['bn']['accum_t'] = result_precision
+    hls_config['LayerName']['linear1']['accum_t'] = result_precision
+    hls_config['LayerName']['linear2']['accum_t'] = result_precision
+
     # result
-    config['Model']['bn']['Precision']['result'] = 'ap_fixed<24, 16>'
-    config['Model']['linear1']['Precision']['result'] = 'ap_fixed<24, 16>'
-    config['Model']['linear2']['Precision']['result'] = 'ap_fixed<24, 16>'
+    hls_config['LayerName']['bn']['Precision']['result'] = reduced_result_precision
+    hls_config['LayerName']['linear1']['Precision']['result'] = result_precision
+    hls_config['LayerName']['linear2']['Precision']['result'] = reduced_result_precision
+
     # reusefactor
-    config['Model']['bn']['ReuseFactor'] = reusefactor
-    config['Model']['linear1']['ReuseFactor'] = reusefactor
-    config['Model']['linear2']['ReuseFactor'] = 500
+    hls_config['LayerName']['bn']['ReuseFactor'] = reusefactor
+    hls_config['LayerName']['linear1']['ReuseFactor'] = reusefactor
+    hls_config['LayerName']['linear2']['ReuseFactor'] = 500
+
     ## activation
-    config['Model']['relu1'] = {}
-    config['Model']['relu2'] = {}
-    config['Model']['relu1']['Precision'] = {}
-    config['Model']['relu2']['Precision'] = {}
-    config['Model']['relu1']['Precision'] = precision
-    config['Model']['relu2']['Precision'] = precision
-    config['Model']['relu1']['ReuseFactor'] = reusefactor
-    config['Model']['relu2']['ReuseFactor'] = reusefactor
-    return config 
+    hls_config['LayerName']['relu1'] = {}
+    hls_config['LayerName']['relu2'] = {}
+    hls_config['LayerName']['relu1']['Precision'] = {}
+    hls_config['LayerName']['relu2']['Precision'] = {}
+    hls_config['LayerName']['relu1']['Precision'] = result_precision
+    hls_config['LayerName']['relu2']['Precision'] = reduced_result_precision
+    hls_config['LayerName']['relu1']['ReuseFactor'] = reusefactor
+    hls_config['LayerName']['relu2']['ReuseFactor'] = reusefactor
+    return hls_config 
 
 
 def main(args):
 
-    base_dir = "hls4ml_prjs"
+    base_dir = "../hls4ml_prjs"
     prj_dir = os.path.join(base_dir, f"hls4ml_prj_resource_rf{args.reuse_factor}")
     hls_fig = os.path.join(prj_dir, "hls_model.png")
 
     model = Classifier()
     print('Loading model checkpoint...')
-    model.load_state_dict(torch.load("checkpoints/checkpoint_tiny_affine.pth"))
+    model.load_state_dict(torch.load("../checkpoints/checkpoint_tiny_affine.pth"))
 
-    # config = hls4ml.utils.config_from_onnx_model(model, granularity="model")
-    # config["Model"]["ReuseFactor"] = args.reuse_factor
-    # config["Model"]["Strategy"] = "resource"
-    # config["Model"]["Precision"] = "ap_fixed<26, 16>"
     hls_config = get_static_config()
 
     print("------------------------------------------------------")
-    print(hls_config)
+    # print_dict(hls_config)
     print("------------------------------------------------------")
 
     hls_model = hls4ml.converters.convert_from_pytorch_model(
@@ -128,24 +138,10 @@ def main(args):
 
     # evaluate hls model
     if args.evaluate:
-        correct = 0
-        total = 0
-
-        for idx in range(400):
-            data, target = test_data[idx]
-
-            data = np.ascontiguousarray(data.numpy())
-
-            states = hls_model.predict(data)
-            target = target.numpy()
-            pred = np.argmax(states).astype(np.int32)
-
-            if pred - target == 0:
-                correct += 1
-            total += 1
+        hls_acc = evaluate_hls(hls_model, test_data)
 
         print("------------------------------------------------------")
-        print(f"hls4ml fidelity: {correct/total:.2f}")
+        print(f"hls4ml fidelity: {hls_acc:.6f}")
         print("------------------------------------------------------")
 
     if args.build:
@@ -156,7 +152,6 @@ def main(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Options for hls4ml synthesis")
     parser.add_argument("--reuse-factor", type=int, default=1)
-    parser.add_argument("-p", "--precision", type=str, default="ap_fixed<16,6>")
     parser.add_argument("-b", "--build", action="store_true")
     parser.add_argument("-e", "--evaluate", action="store_true")
     args = parser.parse_args()
--- utils/config.py
+++ utils/config.py
@@ -1,40 +1,5 @@
 precision = 'ap_fixed<16,6>'
-reusefactor = 120000
-
-config ={}
-
-config['Model'] = {}
-config['Model']['bn'] = {}
-config['Model']['linear1'] = {}
-config['Model']['linear2'] = {}
-config['Model']['bn']['Precision'] = {}
-config['Model']['linear1']['Precision'] = {}
-config['Model']['linear2']['Precision'] = {}
-# weight
-config['Model']['bn']['Precision']['scale'] = precision
-config['Model']['linear1']['Precision']['weight'] = precision
-config['Model']['linear2']['Precision']['weight'] = precision
-# bias
-config['Model']['bn']['Precision']['bias'] = precision
-config['Model']['linear1']['Precision']['bias'] = precision
-config['Model']['linear2']['Precision']['bias'] = precision
-# result
-config['Model']['bn']['Precision']['result'] = precision
-config['Model']['linear1']['Precision']['result'] = precision
-config['Model']['linear2']['Precision']['result'] = precision
-# reusefactor
-config['Model']['bn']['ReuseFactor'] = reusefactor
-config['Model']['linear1']['ReuseFactor'] = reusefactor
-config['Model']['linear2']['ReuseFactor'] = reusefactor
-## activation
-config['Model']['relu1'] = {}
-config['Model']['relu2'] = {}
-config['Model']['relu1']['Precision'] = {}
-config['Model']['relu2']['Precision'] = {}
-config['Model']['relu1']['Precision'] = precision
-config['Model']['relu2']['Precision'] = precision
-config['Model']['relu1']['ReuseFactor'] = reusefactor
-config['Model']['relu2']['ReuseFactor'] = reusefactor
+reusefactor = 1
 
 
 def create_config(layers):
