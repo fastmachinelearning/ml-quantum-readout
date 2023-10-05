a18568f91ba7f5eaaa1f37f1e830de6c22d2a17b
Javier Campos <jivcampos8@gmail.com>
2023-10-05 16:00:02 -0500

    Adding ensemble model

--- training/ensemble-v2.py
+++ training/ensemble-v2.py
@@ -25,57 +25,44 @@ from qkeras.utils import _add_supported_quantized_objects
 class Model(tf.keras.Model):
     def __init__(self, args) -> None:
         super().__init__()
-        # model#1 parameters
+        # Model#1 parameters
         self.model_1_output_shape = args.input_model_output
         self.nodes_fc1 = args.input_model_hidden
         self.num_models = args.num_models
         self.model_1_input_shape = int(args.input_shape / args.num_models)
-        # model#2 parameters
+        # Model#2 parameters
         self.model2_input_shape = args.num_models * self.model_1_output_shape
-        # if True, reuse use model#1 for all input slices 
-        self.reuse = args.reuse
+        # Other training parameters 
+        self.reuse = not args.share_off  # if reuse=True, one copy of Model#1 exists 
         self.quantize = args.quantize
         self.models = self.get_models(args)
 
     def get_model_1(self, input_shape):
+        model = keras.Sequential()
         if self.quantize:
-            model = keras.Sequential()
-            model.add(QDense(input_shape, name='layer1', kernel_quantizer=quantized_bits(16,5,alpha=1), bias_quantizer=quantized_bits(16,5,alpha=1),
-                             kernel_regularizer=regularizers.L1L2(l1=1e-3, l2=1e-2)))
+            model.add(QDense(
+                self.model_1_output_shape, 
+                input_shape=(input_shape,), 
+                name='layer1', 
+                kernel_quantizer=quantized_bits(8,1,alpha=1), 
+                bias_quantizer=quantized_bits(8,1,alpha=1),
+                kernel_regularizer=regularizers.L1L2(l1=1e-3, l2=1e-2))
+            )
             model.add(BatchNormalization(name='batchnorm1'),)
-            model.add(QActivation(activation=quantized_relu(16,6), name='relu1'))
-            model.add(QDense(self.model_1_output_shape, name="layer3", kernel_quantizer=quantized_bits(16,5,alpha=1), bias_quantizer=quantized_bits(16,5,alpha=1),
-                             kernel_regularizer=regularizers.L1L2(l1=1e-3, l2=1e-2)))
-            return model
-        model = keras.Sequential(
-            [
-                layers.Dense(input_shape, activation="relu", name="layer1", kernel_regularizer=regularizers.L1L2(l1=1e-5, l2=1e-4)),
-                layers.Dropout(0.3),
-                # layers.Dense(self.nodes_fc1, name="layer2", kernel_regularizer=regularizers.L1L2(l1=1e-5, l2=1e-4)),
-                layers.Dense(self.model_1_output_shape, name="layer3", kernel_regularizer=regularizers.L1L2(l1=1e-5, l2=1e-4)),
-            ]
-        )
+        else:
+            model.add(Dense(self.model_1_output_shape, input_shape=(input_shape,), name='layer1'))
+            model.add(BatchNormalization(name='batchnorm1'),)
         return model
 
     def get_model_2(self, input_shape):
+        model = keras.Sequential()
         if self.quantize:
-            model = keras.Sequential()
-            model.add(QDense(input_shape, name='layer1', kernel_quantizer=quantized_bits(16,5,alpha=1)))
-            model.add(QActivation(activation=quantized_relu(16,6), name='relu1'))
-            model.add(QDense(2, name='layer2', kernel_quantizer=quantized_bits(16,5,alpha=1)))
-            model.add(Activation(activation='softmax', name='softmax'))
-            return model    
-        model = keras.Sequential(
-            [
-                layers.Dense(input_shape, activation="relu", name="layer1"),
-                layers.Dense(int(input_shape/2), activation="relu", name="layer2"),
-                layers.Dense(2, activation="softmax", name="softmax"),
-            ]
-        )
+            model.add(QDense(2, input_shape=(input_shape,), name='layer1', kernel_quantizer=quantized_bits(8,1,alpha=1)))
+        else:
+            model.add(Dense(2, input_shape=(input_shape,), name="layer1"))
         return model
 
     def get_models(self, args):
-
         models = list()
         for idx in range(args.num_models):
             if idx>0 and self.reuse:
@@ -144,7 +131,6 @@ def one_hot_encode(data):
 
 
 def load_data(args):
-    print(args.data_dir)
     X_train_val = np.load(os.path.join(args.data_dir, 'X_train.npy'))
     X_test = np.load(os.path.join(args.data_dir, 'X_test.npy'))    
     y_train_val = np.load(os.path.join(args.data_dir, 'y_train.npy'))
@@ -152,6 +138,14 @@ def load_data(args):
 
     y_train_val = one_hot_encode(y_train_val)
     y_test = one_hot_encode(y_test)
+
+    print('================Data================')
+    print(f'Loading from {args.data_dir}...')
+    print('Number of Training samples', len(X_train_val))
+    print('\tInput shape:', X_train_val[0].shape)
+    print('Number of Testing samples', len(X_test))
+    print('\tInput shape:', X_test[0].shape)
+    print('====================================')
 
     args.input_shape = X_train_val.shape[1]
     return X_train_val, X_test, y_train_val, y_test
@@ -179,24 +173,16 @@ def main(args):
 
     if args.train:
         model.build(input_shape=(1,1540))
-    #     model.build(input_shape=(1,770))
-    #     print('-----------------------------------------------------')
-    #     print(f'Input Length: {args.input_shape}')
-    #     print(f'Number Models: {len(model.models)-1}')
-    #     print('-----------------------------------------------------')
-    #     print('Input Model')
-    #     print(model.models[0].summary())
-    #     print('\n-----------------------------------------------------')
-    #     print('Output Model')
-    #     print(model.models[-1].summary())
-
         train(model, X_train_val, y_train_val, args)
     
-    if args.ckp:
-        print(f'Saving Model#1 as {args.ckp_file_m1}...')
-        model.models[0].save(args.ckp_file_m1)
-        print(f'Saving Model#2 as {args.ckp_file_m2}...')
-        model.models[-1].save(args.ckp_file_m2)
+    if args.save:
+        args.ckp1 = os.path.join(args.save, args.ckp1)
+        args.ckp2 = os.path.join(args.save, args.ckp2)
+
+        print(f'Saving Model#1 as {args.ckp1}...')
+        model.models[0].save(args.ckp1)
+        print(f'Saving Model#2 as {args.ckp2}...')
+        model.models[-1].save(args.ckp2)
 
     if args.train:
         print('-----------------------------------------------------')
@@ -209,10 +195,9 @@ def main(args):
         print('Output Model')
         print(model.models[-1].summary())
 
-    # load checkpoint and validate model accuracy is the same
-    if args.ckp: 
-        model.load_checkpoint(args.ckp_file_m1, args.ckp_file_m2)
-    # model.load_checkpoint('model_1_b16_i6_ex2.h5', 'model_2_b16_i6_ex2.h5')
+    # Load checkpoint and validate model accuracy is the same
+    if args.save: 
+        model.load_checkpoint(args.ckp1, args.ckp2)
     y_pred = model.predict(X_test)
     model_acc = accuracy_score(np.argmax(y_test, axis=1), np.argmax(y_pred, axis=1))
 
@@ -223,7 +208,7 @@ def main(args):
     print("Model Test Loss [after loading ckp]: {}".format(model_loss))
     print('-----------------------------------------------------')
 
-    # logging test accuracy, loss and parameters
+    # Logging test accuracy, loss and parameters
     if args.log:
         f = open("grid-search-modelv3.txt", "a")
         f.write(f"{model_acc}, {model_loss} - {args}\n")
@@ -235,27 +220,26 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('Training Options.')
-    # model arch parameters 
+    # Model arch parameters 
     parser.add_argument('--num-models', type=int, default=5)
     parser.add_argument('--input-model-hidden', type=int, default=100)
     parser.add_argument('--input-model-output', type=int, default=3)
     parser.add_argument('-q', '--quantize', action='store_true')
-    parser.add_argument('-r', '--reuse', action='store_true')
-    # training parameters
+    parser.add_argument('-s', '--share-off', action='store_true', default=False, 
+                        help='Train different instances of Model#1 (separate copies with different parameters)')
+    # Training parameters
     parser.add_argument('--train', action='store_true')
-    parser.add_argument('-d', '--data-dir', type=str, default='../../data/new-raw-data')
+    parser.add_argument('-d', '--data-dir', type=str, default='../data/new-raw-data-all')
     parser.add_argument('-b', '--batch-size', type=int, default=12800)
-    parser.add_argument('-e', '--epochs',type=int, default=250)
+    parser.add_argument('-e', '--epochs',type=int, default=30)
     parser.add_argument('-v', '--val-split', type=float, default=0.1)
-    parser.add_argument('-lr', type=float, default=1e-3)
-    # logging 
-    parser.add_argument('--ckp', action='store_true')
-    parser.add_argument('--ckp-file-m1', type=str, default='modelv3_1_all.h5')
-    parser.add_argument('--ckp-file-m2', type=str, default='modelv3_2_all.h5')
+    parser.add_argument('-lr', type=float, default=1e-4)
+    # Logging  
+    parser.add_argument('--save', type=str, default=None)
+    parser.add_argument('--ckp1', type=str, default='modelv2_1.h5')
+    parser.add_argument('--ckp2', type=str, default='modelv2_2.h5')
     parser.add_argument('--log', action='store_true', default=False)
     parser.add_argument('--log-file', type=str, default='grid-search-modelv3.txt')
     args = parser.parse_args()
 
     main(args)
-
-# python ensemble.py -d ../../data/all/ -q
