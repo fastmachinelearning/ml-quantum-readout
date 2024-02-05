fe428d0a4f15d9981cf69a5aa2150e6bc2b53731
Javier Campos <jcampos@fnal.gov>
2024-02-04 23:57:51 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,15 @@
-conda activate ml4qick-env
+#!/bin/bash
 
-source /data/Xilinx/Vivado/2020.1/settings64.sh
+if conda activate ml4qick-env; then 
+    echo "Conda environment activated."
+else
+    echo "Conda environment ml4qick-env does not exist. Setting up environment..."
+    conda env create -f environment.yml
+fi
+
+XILINX_DIR="/data/Xilinx/Vivado/2020.1/settings64.sh"
+echo "Setting up Xilinx Vivado."
+
+source $XILINX_DIR
 export XILINXD_LICENSE_FILE=2100@xilinx-lic
 export LM_LICENSE_FILE=2100@xilinx-lic
