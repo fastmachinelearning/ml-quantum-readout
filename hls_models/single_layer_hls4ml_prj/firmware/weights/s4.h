f1c83a0df2ea52d79e4e67c7cd0003fee93ab4b5
Giuseppe Di Guglielmo <gdg@fnal.gov>
2024-03-17 19:29:06 -0700

    Add separate directory with C++ wrapper for an hls4ml project

---
+++ hls_models/single_layer_hls4ml_prj/firmware/weights/s4.h
@@ -0,0 +1,15 @@
+//Numpy array shape [2]
+//Min 0.000245059549
+//Max 0.000249455246
+//Number of zeros 0
+
+#ifndef S4_H_
+#define S4_H_
+
+#ifndef __SYNTHESIS__
+batch_normalization_scale_t s4[2];
+#else
+batch_normalization_scale_t s4[2] = {0.00024945524637587, 0.00024505954934284};
+#endif
+
+#endif
